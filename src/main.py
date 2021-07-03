f24c0bc819d638cfe9d94df8df8d46a157b14a2c
ratoobi <69602231+ratoobi@users.noreply.github.com>
2021-07-03 15:15:56 -0300

    Add tab to the UI containing the merge raw-sessions feature

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 from tibiapal_formatter import format_tibiapal_party_log
+from raw_session_merger import merge_raw_session_logs
 from PyQt5.QtGui import QIcon, QPalette, QColor
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import (
@@ -9,6 +10,7 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
     QApplication,
+    QTabWidget,
 )
 
 
@@ -17,7 +19,8 @@ class SessionsMerger(QWidget):
         super(SessionsMerger, self).__init__()
 
         self.iconName = "../img/icon.ico"
-        self.textbox = None
+        self.raw_sessions_textbox = None
+        self.tibiapal_logs_textbox = None
         self.format_button = None
         self.copy_button = None
         self.paste_button = None
@@ -25,16 +28,23 @@ class SessionsMerger(QWidget):
         self.setWindowTitle("Party Sessions Merger")
         self.setWindowIcon(QIcon(self.iconName))
         self.setMinimumSize(QSize(880, 680))
-        self.init_ui()
 
-    def init_ui(self):
+        layout = QVBoxLayout()
+        tabs = QTabWidget()
+        tabs.addTab(self.raw_sessions_tab_ui(), "Raw Party Sessions")
+        tabs.addTab(self.tibiapal_logs_tab_ui(), "TibiaPal LootSplit Logs")
+        layout.addWidget(tabs)
+        self.setLayout(layout)
+
+    def raw_sessions_tab_ui(self):
+        raw_sessions_tab = QWidget()
+
         outer_layout = QVBoxLayout()
         buttons_layout = QVBoxLayout()
         bottom_buttons_layout = QHBoxLayout()
 
-        self.textbox = QPlainTextEdit(self)
-        self.textbox.resize(200, 200)
-        outer_layout.addWidget(self.textbox)
+        self.raw_sessions_textbox = QPlainTextEdit(self)
+        outer_layout.addWidget(self.raw_sessions_textbox)
 
         self.format_button = QPushButton(self)
         self.format_button.setText("Merge")
@@ -54,18 +64,58 @@ class SessionsMerger(QWidget):
         buttons_layout.addLayout(bottom_buttons_layout)
         outer_layout.addLayout(buttons_layout)
         self.setLayout(outer_layout)
+        raw_sessions_tab.setLayout(outer_layout)
+
+        return raw_sessions_tab
+
+    def tibiapal_logs_tab_ui(self):
+        tibiapal_logs_tab = QWidget()
+
+        outer_layout = QVBoxLayout()
+        buttons_layout = QVBoxLayout()
+        bottom_buttons_layout = QHBoxLayout()
+
+        self.tibiapal_logs_textbox = QPlainTextEdit(self)
+        outer_layout.addWidget(self.tibiapal_logs_textbox)
+
+        self.format_button = QPushButton(self)
+        self.format_button.setText("Merge")
+        self.format_button.clicked.connect(self.tibiapal_merge_button_handler)
+        buttons_layout.addWidget(self.format_button)
+
+        self.paste_button = QPushButton(self)
+        self.paste_button.setText("Paste")
+        self.paste_button.clicked.connect(self.paste_button_handler)
+        bottom_buttons_layout.addWidget(self.paste_button)
+
+        self.copy_button = QPushButton(self)
+        self.copy_button.setText("Copy")
+        self.copy_button.clicked.connect(self.copy_button_handler)
+        bottom_buttons_layout.addWidget(self.copy_button)
+
+        buttons_layout.addLayout(bottom_buttons_layout)
+        outer_layout.addLayout(buttons_layout)
+        self.setLayout(outer_layout)
+        tibiapal_logs_tab.setLayout(outer_layout)
+
+        return tibiapal_logs_tab
 
     def merge_button_handler(self):
-        input_text = self.textbox.toPlainText()
+        input_text = self.raw_sessions_textbox.toPlainText()
+        output_text = merge_raw_session_logs(input_text)
+        self.raw_sessions_textbox.setPlainText(output_text)
+
+    def tibiapal_merge_button_handler(self):
+        input_text = self.tibiapal_logs_textbox.toPlainText()
         output_text = format_tibiapal_party_log(input_text)
-        self.textbox.setPlainText(output_text)
+        self.tibiapal_logs_textbox.setPlainText(output_text)
 
     def paste_button_handler(self):
-        self.textbox.paste()
+        self.raw_sessions_textbox.paste()
 
     def copy_button_handler(self):
-        self.textbox.selectAll()
-        self.textbox.copy()
+        self.raw_sessions_textbox.selectAll()
+        self.raw_sessions_textbox.copy()
 
 
 if __name__ == "__main__":
